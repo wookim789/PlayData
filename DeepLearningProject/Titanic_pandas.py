3b36d90e1943d9d8deca69103fac41d16166e482
wookim789 <wookim789@gmail.com>
2018-05-03 09:55:05 +0900

    java_source_modify

--- DeepLearningProject/Titanic_pandas.py
+++ DeepLearningProject/Titanic_pandas.py
@@ -18,6 +18,8 @@ testFrame = DataFrame(testFile)
 testAnswerFrame = DataFrame(testAnswerFile)
 
 
+trainAnswerFrame = trainFrame['Survived']
+
 del trainFrame['PassengerId']
 del trainFrame['Survived']
 del trainFrame['Ticket']
@@ -35,7 +37,7 @@ trainFrame = trainFrame.replace(np.nan,0)
 sex = lambda x : 0 if x == 'male' else 1
 age = lambda x : 0.7 if x == 0 else 1 if x <= 19 else 0.5 if x <= 30 else 0.3 if x <=50 else 0.8
 pclass = lambda x : 0 if x == 3 else 0.5 if x == 2 else 1
-fare = lambda x : x/512.
+fare = lambda x : x / 512.
 
 
 print(trainFrame)
@@ -43,72 +45,49 @@ print(trainFrame.describe())
 #print(trainFrame.ix[:,0].value_counts())
 
 
-
 # #,Fare,Cabin,Embarked
 
-# testFrame.PassengerId = testFrame.PassengerId.replace(np.nan,1)
-# testFrame.Pclass = testFrame.Pclass.replace(np.nan,3)
-# testFrame.Sex = testFrame.Sex.replace(np.nan,replaceNanInSex)
-# testFrame.Sex = testFrame.Sex.replace('female',0)
-# testFrame.Sex = testFrame.Sex.replace('male',1)
-# testFrame.Age = testFrame.Age.replace(np.nan,29)
-# testFrame.Fare= testFrame.Fare.replace(np.nan,meanFare)
-# testFrame.Cabin = testFrame.Cabin.replace(np.nan,"unknown")
-# testFrame.SibSp = testFrame.SibSp.replace(np.nan,0)
-# testFrame.Parch = testFrame.Parch.replace(np.nan,0)
-# testFrame.Embarked = testFrame.Embarked.replace(np.nan,replaceNanInEmvarked)
-# testFrame.Embarked = testFrame.Embarked.replace('S',1)
-# testFrame.Embarked = testFrame.Embarked.replace('C',2)
-# testFrame.Embarked = testFrame.Embarked.replace('Q',3)
+testAnswerFrame.Pclass = testAnswerFrame.Pclass.apply(pclass)
+testFrame.Sex = testFrame.Sex.apply(sex)
+testFrame.Age = testFrame.Age.apply(age)
+testFrame.Fare= testFrame.Fare.apply(fare)
 
-# trainAnswerFrame = trainFrame['Survived']
 
-# trainFrame.PassengerId = trainFrame.PassengerId.replace(np.nan,1)
-# trainFrame.Survived = trainFrame.Survived.replace(np.nan,1)
-# trainFrame.Pclass = trainFrame.Pclass.replace(np.nan,3)
-# trainFrame.Sex = trainFrame.Sex.replace(np.nan,replaceNanInSex)
-# trainFrame.Sex = trainFrame.Sex.replace('female',0)
-# trainFrame.Sex = trainFrame.Sex.replace('male',1)
-# trainFrame.Age = trainFrame.Age.replace(np.nan,29)
-# trainFrame.Fare= trainFrame.Fare.replace(np.nan,meanFare)
-# trainFrame.Cabin = trainFrame.Cabin.replace(np.nan,"unknown")
-# trainFrame.SibSp = trainFrame.SibSp.replace(np.nan,0)
-# trainFrame.Parch = trainFrame.Parch.replace(np.nan,0)
-# trainFrame.Embarked = trainFrame.Embarked.replace(np.nan,replaceNanInEmvarked)
-# trainFrame.Embarked = trainFrame.Embarked.replace('S',1)
-# trainFrame.Embarked = trainFrame.Embarked.replace('C',2)
-# trainFrame.Embarked = trainFrame.Embarked.replace('Q',3)
+trainFrame.Pclass = trainFrame.Pclass.apply(pclass)
+trainFrame.Sex = trainFrame.Sex.apply(sex)
+trainFrame.Age = trainFrame.Age.apply(age)
+trainFrame.Fare= trainFrame.Fare.apply(fare)
 
 
 # #print(trainFrame.ix[:,6])
 # #train data 제거 항목
 
 
-# #keras
+#keras
 # print(trainFrame.shape)
 # print(testFrame.shape)
-# model = Sequential()
-# model.add(Dense(units = 100, input_shape =(8,) , activation = 'relu'))
-# model.add(Dropout(0.2))
-# #model.add(Dense(units = 60, activation = 'relu'))
-# #model.add(Dropout(0.6))
-# model.add(Dense(units=1, activation='sigmoid'))
+model = Sequential()
+model.add(Dense(units = 100, input_shape =(8,) , activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(units = 60, activation = 'relu'))
+model.add(Dropout(0.6))
+model.add(Dense(units=1, activation='sigmoid'))
 
-# model.compile(loss='binary_crossentropy',
-#               optimizer = 'adam',
-#               metrics=['accuracy']
-#              )
-# model.fit(trainFrame, trainAnswerFrame, epochs=30,  validation_split = 0.2)
+model.compile(loss='binary_crossentropy',
+               optimizer = 'adam',
+               metrics=['accuracy']
+              )
+model.fit(trainFrame, trainAnswerFrame, epochs=30,  validation_split = 0.2)
 
-# model.evaluate(testFrame, testAnswerFrame)
+model.evaluate(testFrame, testAnswerFrame)
 
-# predicted_Y = model.predict(testFrame)
-# match = predicted_Y == testAnswerFrame
-# wrong_label = np.where(predicted_Y>0.5, 1,0)
-# cnt = 0
+predicted_Y = model.predict(testFrame)
+match = predicted_Y == testAnswerFrame
+wrong_label = np.where(predicted_Y>0.5, 1,0)
+cnt = 0
 
-# taf = np.array(testAnswerFile)
-# for i in range (wrong_label.shape[0]) :
-#     if wrong_label[i] == taf[i] :
-#         cnt = cnt + 1
-# print("acc : {:d}",  (cnt/wrong_label.shape[0]))
+taf = np.array(testAnswerFile)
+for i in range (wrong_label.shape[0]) :
+    if wrong_label[i] == taf[i] :
+        cnt = cnt + 1
+print("acc : {:d}",  (cnt/wrong_label.shape[0]))
